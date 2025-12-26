ca5fb12c3b0ef86d2cdb7b01ce5da5761d0cce18
Miguel Gomez <himiggu@gmail.com>
2025-12-26 18:04:32 +0100

    Add basic HeroHeader and LessonTable render tests

---
+++ test/HeroHeader.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { HeroHeader } from "../src/components/HeroHeader";
+
+const props = {
+  title: "Your Title",
+  description: "Your Description",
+  badge: "alpha",
+};
+
+describe("HeroHeader", () => {
+  it("renders title, description, and badge", () => {
+    render(<HeroHeader {...props} />);
+    expect(
+      screen.getByRole("heading", { name: /your title/i })
+    ).toBeInTheDocument();
+    expect(screen.getByText(/your description/i)).toBeInTheDocument();
+    expect(screen.getByText(/alpha/i)).toBeInTheDocument();
+  });
+});
---
+++ test/LessonTable.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import { describe, it, expect, vi, afterEach } from "vitest";
+import { LessonTable } from "../src/components/LessonTable";
+
+const props = {
+  lessons: [
+    {
+      id: "0-0-How to Get Help",
+      title: "How to Get Help",
+      duration: "01:04",
+      section: "Getting Started with Typescript",
+    },
+    {
+      id: "0-1-Join Our Community!",
+      title: "Join Our Community!",
+      duration: "00:07",
+      section: "Getting Started with Typescript",
+    },
+    {
+      id: "0-2-Course Resources",
+      title: "Course Resources",
+      duration: "00:38",
+      section: "Getting Started with Typescript",
+    },
+  ],
+  onRemoveSection: vi.fn(),
+  onRemoveLesson: vi.fn(),
+};
+
+describe("LessonTable", () => {
+  afterEach(() => {
+    cleanup();
+  });
+  it("it's showing Episodes & Durations", () => {
+    render(<LessonTable {...props} />);
+    const HeaderElement = screen.getByRole("heading", {
+      name: /episodes & durations/i,
+    });
+    expect(HeaderElement).toBeInTheDocument();
+    expect(
+      screen.getByText(/3\s*items/i, { selector: ".pill" })
+    ).toBeInTheDocument();
+  });
+  it("works when user types and then clicks", async () => {});
+});
